ef2002cf55cbb97ae6a39e127dd0d4e88cff5da4
omerkoseoglu <omerkoseoglu@outlook.com>
2020-01-03 22:28:03 +0300

    Bişeyler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { ContactComponent } from './components/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  yemekFormu: any;
+  yemekler: string[] = [
+    "Mercimek Çorbası",
+    "Patlıcan kebabı",
+    "Mantı"
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.yemekFormu = this.formBuilder.group({
+      yemekAdi: 'adfasfaf'
+    });
+
+    console.log(this.yemekler);
+  }
 
   ngOnInit() {
+  }
+
+  kaydet() {
+    this.yemekler.push(this.yemekFormu.controls.yemekAdi.value);
+    console.log(this.yemekFormu.controls.yemekAdi.value);
+    console.log('Buton çalışıyor.');
   }
 
 }
